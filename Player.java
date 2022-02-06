bb656c064931ac863372cbf0e2623a4623334ac1
XhenetaHasanii <93622223+XhenetaHasanii@users.noreply.github.com>
2022-02-06 20:39:36 +0100

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player{
+    private String name;
+    private  char symbol;
+
+    public Player( String name,char symbol){
+        this.name=name;
+        this.symbol=symbol;
+    }
+
+
+}
